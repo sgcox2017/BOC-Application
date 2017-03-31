ce81dae33fa40a841b5f79f4a659c3ceb6776f5d
sgcox2017 <sgcox2017@spartans.manchester.edu>
2017-03-31 14:56:10 -0400

    Centralized All Files into One Project

---
+++ BOC_Application/public_html/JavaScript/login.js
@@ -0,0 +1,11 @@
+
+function showCreateAccount() {
+    document.getElementById("forgotPassword").style.visibility = "hidden";
+    document.getElementById("newAccount").style.visibility = "visible";
+}
+
+function showForgotPassword() {
+    document.getElementById("newAccount").style.visibility = "hidden";
+    document.getElementById("forgotPassword").style.visibility = "visible";
+}
+
