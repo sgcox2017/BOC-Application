428dd6877e55080217c4a5ac7aac7b7a38c9ff8f
sgcox2017 <sgcox2017@spartans.manchester.edu>
2017-04-07 12:39:32 -0400

    Updated Login JavaScript

--- BOC_Application/public_html/JavaScript/login.js
+++ BOC_Application/public_html/JavaScript/login.js
@@ -1,11 +1,38 @@
 
 function showCreateAccount() {
-    document.getElementById("forgotPassword").style.visibility = "hidden";
-    document.getElementById("newAccount").style.visibility = "visible";
+    document.getElementById("login").style.display="none";
+    document.getElementById("username").value="";
+    document.getElementById("password").value="";
+    document.getElementById("forgotPassword").style.display="none";
+    document.getElementById("forgotUsername").value="";
+    document.getElementById("newAccount").style.display="block";
 }
 
 function showForgotPassword() {
-    document.getElementById("newAccount").style.visibility = "hidden";
-    document.getElementById("forgotPassword").style.visibility = "visible";
+    document.getElementById("newAccount").style.display="none";
+    document.getElementById("newUsername").value="";
+    document.getElementById("newPassword").value="";
+    document.getElementById("newConfirmPassword").value="";
+    document.getElementById("newEmail").value="";
+    document.getElementById("newSecurityQuestion").value="";
+    document.getElementById("newSecurityAnswer").value="";
+    document.getElementById("password").value="";
+    document.getElementById("login").style.display="none";
+    document.getElementById("username").value="";
+    document.getElementById("password").value="";
+    document.getElementById("forgotPassword").style.display="block";
+}
+
+function showLogin() {
+    document.getElementById("forgotPassword").style.display="none";
+    document.getElementById("forgotUsername").value="";
+    document.getElementById("newAccount").style.display="none";
+    document.getElementById("newUsername").value="";
+    document.getElementById("newPassword").value="";
+    document.getElementById("newConfirmPassword").value="";
+    document.getElementById("newEmail").value="";
+    document.getElementById("newSecurityQuestion").value="";
+    document.getElementById("newSecurityAnswer").value="";
+    document.getElementById("login").style.display="block";
 }
 
