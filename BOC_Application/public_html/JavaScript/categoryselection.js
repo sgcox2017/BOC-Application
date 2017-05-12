a48045c2ab0481afdbced0c18ca258b498f35a49
CSReinhold2019 <CSReinhold2019@manchester.edu>
2017-05-12 13:38:08 -0400

    CatSelection (Needs work)

---
+++ BOC_Application/public_html/JavaScript/categoryselection.js
@@ -0,0 +1,44 @@
+
+char buttonone =0;
+char buttontwo = 0;
+char buttonthree = 0;
+char buttonfour = 0;
+char buttonfive = 0; 
+char buttonsix = 0;
+
+
+function pressOne(){
+	
+	buttonone=1;
+	
+}
+
+function pressTwo(){
+	
+	buttontwo = 1;
+	
+}
+
+function pressThree(){
+	
+	buttonthree = 1;
+	
+}
+
+function pressFour(){
+	
+	buttonfour =1;
+	
+}
+
+function pressFive(){
+	
+	buttonfive = 1;
+	
+}
+
+function pressSix(){
+	
+	buttonsix = 1;
+	
+}
